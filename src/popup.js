96477abd11ce23c3831a3b82e50b2c71ac9c792a
Aidan Kane <kaneaidan12@gmail.com>
2024-02-10 17:02:08 -0500

    Make popup form with html and send response from popup.js to background

---
+++ src/popup.js
@@ -0,0 +1,12 @@
+// This event listener will be triggered when the form is submitted
+document.getElementById('keywordForm').addEventListener('submit', function(event) {
+    event.preventDefault();  // Prevents page from reloading on submit
+
+    var keyword = document.getElementById('keywordInput').value;    // Get input value for keyword
+
+    // Sends message to background script with keyword
+    chrome.runtime.sendMessage({ keyword: keyword }, function(response) {
+        console.log('Keyword sent to background: ' , keyword);
+        // can handle response from background script here
+    });
+});
